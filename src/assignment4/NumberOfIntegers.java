605db2a16c2de5c059226a816332617f4c22f403
busingepius <busingepius@gmail.com>
2024-01-20 20:17:02 -0600

    assignment 4

---
+++ src/assignment4/NumberOfIntegers.java
@@ -0,0 +1,15 @@
+/**
+ * Created by: Businge Pius
+ * Company : Gegabox
+ * Year : 2024
+ * Date : 20/01/2024
+ * Time : 20:15
+ * Project Name : assignment4
+ */
+package assignment4;
+
+public class NumberOfIntegers {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/assignment4/ReverseDigit.java
@@ -0,0 +1,21 @@
+/**
+ * Created by: Businge Pius
+ * Company : Gegabox
+ * Year : 2024
+ * Date : 20/01/2024
+ * Time : 20:05
+ * Project Name : assignment4
+ */
+package assignment4;
+
+public class ReverseDigit {
+    public static void main(String[] args) {
+
+    }
+    public static void reverseDigits(int number){
+        if (number <=0 ){
+            return;
+        }
+
+    }
+}
